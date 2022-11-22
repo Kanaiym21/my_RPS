2618ec336b9b9dccf6e64b288e7cc9ad6ac7b937
Kanaiym <abdigak@clarkson.edu>
2022-11-22 11:37:04 -0500

    changed spelling mistake, wrote javascript for text results at the end of the game

--- js/page.js
+++ js/page.js
@@ -2,7 +2,7 @@
     sessionStorage.playerschoice = x;
     var y = Math.ceil(Math.random() *3);
 if (y == 1) { //if y equals 1 it means rock
-    sessionStorage.opponentsschoice = 'rock'; 
+    sessionStorage.opponentschoice = 'rock'; 
 }
 else if (y == 2) { //if y equals 2 it means paper
     sessionStorage.opponentsschoice = 'paper'
@@ -10,22 +10,34 @@ else if (y == 2) { //if y equals 2 it means paper
 else if (y == 3) { //if y equals 3 it means scissors
     sessionStorage.opponentsschoice = 'scissors'
 }
-if ((sessionStorage.playerschoice == 'rock') && (sessionStorage.opponentsschoice == 'paper')) {
+if ((sessionStorage.playerschoice == 'rock') && (sessionStorage.opponentschoice == 'paper')) {
 sessionStorage.result = 'lose';
 }
-else if ((sessionStorage.playerschoice == 'rock') && (sessionStorage.opponentsschoice == 'scissors')){
+else if ((sessionStorage.playerschoice == 'rock') && (sessionStorage.opponentschoice == 'scissors')){
     sessionStorage.result = 'won';   
 }
-else if ((sessionStorage.playerschoice == 'paper') && (sessionStorage.opponentsschoice == 'scissors')){
+else if ((sessionStorage.playerschoice == 'paper') && (sessionStorage.opponentschoice == 'scissors')){
     sessionStorage.result = 'lose';   
 }
-else if ((sessionStorage.playerschoice == 'paper') && (sessionStorage.opponentsschoice == 'rock')){
+else if ((sessionStorage.playerschoice == 'paper') && (sessionStorage.opponentschoice == 'rock')){
     sessionStorage.result = 'won';   
 }
-else if ((sessionStorage.playerschoice == 'scissors') && (sessionStorage.opponentsschoice == 'rock')){
+else if ((sessionStorage.playerschoice == 'scissors') && (sessionStorage.opponentschoice == 'rock')){
     sessionStorage.result = 'lose';   
 }
-else if ((sessionStorage.playerschoice == 'scissors') && (sessionStorage.opponentsschoice == 'paper')){
+else if ((sessionStorage.playerschoice == 'scissors') && (sessionStorage.opponentschoice == 'paper')){
     sessionStorage.result = 'won';   
+}
+else if (sessionStorage.playerschoice == sessionStorage.opponentschoice) { //if my choice and opponents choice equel it is tie
+sessionStorage.result = 'tie';
+}
+if (sessionStorage.result == 'lose') {
+document.getElementById('result').innerHTML='You lost.It was unfortunate';
+}
+else if (sessionStorage.result == 'won') {
+    document.getElementById('result').innerHTML='Good job. You won!';
+ }
+else if (sessionStorage.result == 'tie') {
+    document.getElementById('result').innerHTML='It is tie. Try again!';
 }
  }
